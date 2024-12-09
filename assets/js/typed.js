2f8088ef2f447d2e409cf1e852ee174b7f63b89e
Dead <dawdawdawd02@gmail.com>
2024-12-09 20:22:23 +1400

    Delete assets directory

--- assets/js/typed.js
+++
@@ -1,7 +0,0 @@
-new Typed('#typed',{
-    strings : [,'Biolink ig','Screams.cc >>>','who are you?','why am i here','#catchin felonys like pokimon'],
-    typeSpeed : 35,
-    delaySpeed : 90,
-    showCursor: false,
-    loop : true
-  })
